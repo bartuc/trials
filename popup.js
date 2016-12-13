56188faa3d992265e3fd32d70f9fd37ec6b8c29c
Onur Catakoglu <mronur89@gmail.com>
2016-12-13 15:46:55 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,9 +1,1 @@
-function myFunction() {
-    var x;
-    if (confirm("Press a button!") == true) {
-        x = "You pressed OK!";
-    } else {
-        x = "You pressed Cancel!";
-    }
-    document.getElementById("demo").innerHTML = x;
-}
+alert("I am an alert box!");
